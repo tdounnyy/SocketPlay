e57a7688153f3c67779fcbebc2b4c07b2faeac7a
Yufei Duan <yufei.duan@shopee.com>
2020-07-02 20:50:20 +0800

    Remove sources

--- src/main/java/felix/duan/socket/IEntryPoint.java
+++
@@ -1,7 +0,0 @@
-package felix.duan.socket;
-
-import java.net.UnknownHostException;
-
-public interface IEntryPoint {
-    public void start() throws UnknownHostException;
-}
--- src/main/java/felix/duan/socket/util/Consts.java
+++
@@ -1,5 +0,0 @@
-package felix.duan.socket.util;
-
-public interface Consts {
-    public int PORT = 9876;
-}
